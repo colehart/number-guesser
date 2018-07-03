fd0e6ff1694214b16af08fadbc8eeb0195b64a94
Cole Hart <cole@colehart.com>
2018-07-03 09:02:27 -0600

    added css (needs refactoring) and javascript functionality (needs range delimiters

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-// Structure:
+var randomNumber = 0;
+var userInput = document.querySelector('#userGuess');
+var clearButton = document.querySelector('.right-btn');
+var guessButton = document.querySelector('.left-btn');
+var resetButton = document.querySelector('.big-btn');
 
-// query selectors
+window.addEventListener('load', generateAnswer);
+guessButton.addEventListener('click', checkGuess); 
+resetButton.addEventListener('click', restartGame);
+clearButton.addEventListener('click', clearInput);
 
-// event listeners
+function generateAnswer() {
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
 
-// functions
+function checkGuess(event) {
+  event.preventDefault();
+  var guess = parseInt(userInput.value);
+  document.querySelector('.your-guess').innerText = guess;
+  if (guess < randomNumber) {
+    document.querySelector('.message').innerText = 'That is too low';
+  } else if (guess > randomNumber) {
+    document.querySelector('.message').innerText = 'That is too high';
+  } else {
+    document.querySelector('.message').innerText = 'BOOM';
+  }
+}
+
+function clearInput() {
+  userInput.value = "";
+}
+
+
+function restartGame() {
+  generateAnswer();
+  clearInput();
+  document.querySelector('.your-guess').innerText = '?';
+  document.querySelector('.message').innerText = 'Make a guess';
+
+}
